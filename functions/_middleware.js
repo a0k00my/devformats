889072a53a5aafff2e9398db8d2fe62dd8831b30
a0k00my <adityanitjsr@gmail.com>
2026-07-25 15:20:38 +0530

    Add Pages Function to noindex the pages.dev domain
    Prevents duplicate-content indexing of devformats.pages.dev by adding
    X-Robots-Tag: noindex on that host only, leaving devformats.com untouched.

---
+++ functions/_middleware.js
@@ -0,0 +1,16 @@
+export async function onRequest(context) {
+  const response = await context.next();
+  const host = new URL(context.request.url).hostname;
+
+  if (host.endsWith('.pages.dev')) {
+    const headers = new Headers(response.headers);
+    headers.set('X-Robots-Tag', 'noindex, nofollow');
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  }
+
+  return response;
+}
